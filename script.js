073b67e44c96020499d1f4d551124123ecb67224
sourov ch <sourov@mx>
2023-04-13 01:09:40 +0600

    close nav on outsite click

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const dd = document.querySelectorAll(".dropdown");
 const menuToggle = document.getElementById("menu-toggle");
 const menuBar = document.getElementById('menubar');
 const pageContents = document.getElementById('page-contents');
+const navCloser = document.querySelector('.nav-closer')
 
 dd.forEach((i) => {
   i.onclick = (e) => {
@@ -13,8 +14,14 @@ dd.forEach((i) => {
 
 menuToggle.onclick = () => {
     menuBar.classList.toggle('active');
+    navCloser.classList.toggle('active');
     menuBar.classList.remove('p-absolute');
     pageContents.classList.add("m0");
+}
+
+navCloser.onclick = () => {
+  menuBar.classList.remove('active');
+  navCloser.classList.remove('active');
 }
 
 menuBar.ontransitionend = () => {
