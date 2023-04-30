b24d99a3bafe3854c393a4b652654947f1c31e6b
sourov ch <sourov@mx>
2023-04-30 11:35:59 +0600

    new pages remaning

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 import { MainSec } from "./components/mainSec.js";
 customElements.define("main-sec", MainSec);
 
-function print(e) {
-  print();
+
+window.openModule = (e, id) => {
+  e.preventDefault();
+
+  const modal = document.getElementById(id);
+
+  modal.classList.toggle("active");
+}
+
+window.closeModal = (e, id) => {
+  e.preventDefault();
+
+  const modal = document.getElementById(id);
+
+  modal.classList.remove("active");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -11,10 +24,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const pageContents = document.getElementById("page-contents");
   const navCloser = document.querySelector(".nav-closer");
   const menuToggle = document.getElementById("menu-toggle");
-  const fileSelector = document.querySelectorAll(".form-wrapper input[type='file']")
-  const addBtn = document.getElementById('add-btn');
-  const addForm = document.getElementById('add-form');
-  const addFormClose = document.getElementById('add-form-close');
+  const fileSelector = document.querySelectorAll(
+    ".form-wrapper input[type='file']"
+  );
+  const addBtn = document.getElementById("add-btn");
+  const addForm = document.getElementById("add-form");
+  const addFormClose = document.getElementById("add-form-close");
+  const multiSelectList = document.querySelectorAll(".multi-select");
 
   dd.forEach((i) => {
     i.onclick = () => {
@@ -44,20 +60,80 @@ document.addEventListener("DOMContentLoaded", () => {
 
   fileSelector.forEach((elem) => {
     elem.onchange = (e) => {
-      console.log("ok")
+      console.log("ok");
       e.target.parentElement.dataset.filename = e.target.files[0].name || "";
-    }
-  })
+    };
+  });
 
-  addBtn?.addEventListener('click', (e) => {
-    addForm?.classList.toggle('active');
-  })
+  addBtn?.addEventListener("click", (e) => {
+    addForm?.classList.toggle("active");
+  });
 
-  addFormClose?.addEventListener('click', (e) => {
+  addFormClose?.addEventListener("click", (e) => {
     e.preventDefault();
-    
-    addForm?.classList.remove('active');
-  })
+
+    addForm?.classList.remove("active");
+  });
 
   document.getElementById("logout-btn").onclick = (e) => console.log("Log Out");
+
+  multiSelectList.forEach((multiSelect) => {
+    const parentElement = multiSelect.parentElement;
+    const options = multiSelect.querySelectorAll("select option");
+    options[0].selected = true;
+
+    const selectedCont = parentElement.querySelector(".selected-list");
+    const selectDD = parentElement.querySelector(".select-dropdown");
+
+    const selectData = findSelected(options);
+    populate(selectedCont, selectData.selected);
+    populate(selectDD, selectData.notSelected);
+
+    multiSelect.onclick = () => {
+      selectDD.classList.toggle("active");
+    };
+
+    function populate(cont, items = []) {
+      cont.innerHTML = "";
+      items.forEach((item) => {
+        const div = document.createElement("div");
+        div.dataset.index = item.index;
+        div.innerText = item.text;
+
+        div.onclick = (e) => {
+          e.stopPropagation();
+
+          if (options[item.index].selected) {
+            options[item.index].selected = false;
+          } else {
+            options[item.index].selected = true;
+          }
+
+          const selectData = findSelected(options);
+          populate(selectedCont, selectData.selected);
+          populate(selectDD, selectData.notSelected);
+        };
+
+        cont.appendChild(div);
+      });
+    }
+
+    function findSelected(options) {
+      const selected = [];
+      const notSelected = [];
+
+      options.forEach((option, index) => {
+        if (option.selected) {
+          selected.push({ text: option.innerText, index });
+        } else {
+          notSelected.push({ text: option.innerText, index });
+        }
+      });
+
+      return {
+        selected,
+        notSelected,
+      };
+    }
+  });
 });
